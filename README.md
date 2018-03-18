04aed644e6c9c4bbc7ecddef3a30bbad9b483ac6
Tatiana Karabut <tyulalex@gmail.com>
2018-03-19 00:46:58 +0300

    github trending script

--- README.md
+++ README.md
@@ -1,6 +1,29 @@
 # Github Trends
 
-[TODO. There will be project description]
+Script designed to list 20 most trending repository created within last 7 days.
+Script lists these repositories sorted descending by stars with amount of open issues for each of repo. 
+
+# How to Install
+
+Python 3 should be already installed. Then use pip (or pip3 if there is a conflict with old Python 2 setup) to install dependencies:
+
+```bash
+pip install -r requirements.txt # alternatively try pip3
+```
+
+Remember, it is recommended to use [virtualenv/venv](https://devman.org/encyclopedia/pip/pip_virtualenv/) for better isolation.
+
+
+# Code example
+```
+python github_trending.py  #alternatively try python3
+
+https://github.com/ambv/black The uncompromising Python code formatter  | Open Issues: 15
+https://github.com/iOS-Strikers/OCBarrage iOS 弹幕库 OCBarrage, 同时渲染5000条弹幕也不卡, 轻量, 可拓展, 高度自定义动画, 超高性能, 简单易上手; A barrage render-engine with high performance for iOS. At the same time, rendering 5000 barrages is also very smooth, lightweight, scalable, highly custom animation, ultra high performance, simple and easy to use!  | Open Issues: 0
+https://github.com/normandipalo/faceID_beta An implementation of iPhone X's FaceID using face embeddings and siamese networks on RGBD images.  | Open Issues: 1
+....
+....
+```
 
 # Project Goals
 
--- github_trending.py
+++ github_trending.py
@@ -1,11 +1,42 @@
+import requests
+from datetime import datetime, timedelta
 
 
-def get_trending_repositories(top_size):
-    pass
+def get_trending_repositories(top_size, days_ago=7):
+    since_date_dt = datetime.now().date() - timedelta(days=days_ago)
+    since_date_str = datetime.strftime(since_date_dt, "%Y-%m-%d")
+    url = '{}'.format('https://api.github.com/search/repositories')
+    response = requests.get(
+        url=url,
+        params={
+            'q': 'created:>{since_date} sort:stars'.format(
+                since_date=since_date_str)
+        }
+    )
+    if response.ok:
+        return response.json()['items'][:top_size - 1]
+    return []
 
 
-def get_open_issues_amount(repo_owner, repo_name):
-    pass
+def get_open_issues_amount(issues_url):
+    open_issues_amount = 0
+    issues_response = requests.get(issues_url.replace('{/number}', ''))
+    if not issues_response.ok:
+        return None
+    for issue in issues_response.json():
+        if not issue.get('pull_request'):
+            open_issues_amount += 1
+    return open_issues_amount
+
 
 if __name__ == '__main__':
-    pass
+    top_size = 20
+    trending_repositories = get_trending_repositories(top_size)
+    for repo in trending_repositories:
+        if repo['has_issues']:
+            open_issues_amount = get_open_issues_amount(repo['issues_url'])
+        print(
+            repo['html_url'],
+            repo['description'],
+            " | Open Issues: {}".format(open_issues_amount)
+        )
